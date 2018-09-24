0ae6b13813457fffc749cb160f5a01288cc36d14
StarRuCat <starrucat@gmail.com>
2018-09-24 17:56:53 +0300

    Обновление

---
+++ C# lvl1/Lession2/Lession2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Lession2
+{
+    class Program
+    {
+        static void Main()
+        {
+        }
+    }
+}
