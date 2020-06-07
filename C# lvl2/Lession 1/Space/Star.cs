dc85b390ef2f79305a8a1deb8d7f05b513d6163a
Fedor <starrucat@gmail.com>
2020-06-07 14:37:38 +0300

    Первая попытка выполнить первое задание
    Все плохо, помогите

---
+++ C# lvl2/Lession 1/Space/Star.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+
+namespace MyGame
+{
+    class Star : BaseObject
+    {
+        public Star(Point pos, Point dir, Size size) : base(pos, dir, size)
+        {
+        }
+    }
+}
